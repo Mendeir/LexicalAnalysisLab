f8535f2d0fbf61f74223ede198689128875e3ef4
Lorenze Dionell K. Arroyo <90163665+lorenzeArroyo@users.noreply.github.com>
2022-05-03 00:15:55 +0800

    Added functions to evaluate & display punctuators

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@
 using namespace std;
 
 string retrieveFile(string sourceFile);
+bool isPunctuator(string code, int i);
+void dispPunctuators(string code);
 
 int main() {
 
     string fileName = "code.txt";
     string code = retrieveFile(fileName);
-    cout << code;
+    cout << "code.txt:\n" << code << "\n\n";
+
+    dispPunctuators(code);
 
     return 0;
 }
@@ -34,4 +38,142 @@ string retrieveFile(string sourceFile) {
     }
 
     return code;
+}
+
+void dispPunctuators(string code){
+    //Scanner for punctuators
+    //Mainly for testing before merge
+    for(int i=0; i<code.length(); i++){
+            if(isPunctuator(code, i)){
+                cout << code[i] << " is a Punctuator\n";
+            }
+        }
+}
+
+bool isPunctuator(string code, int i){
+    /*
+        Returns true if char is a punctuator
+        EXCLUDES operators that use punctuator characters
+        except exact matches (i.e. punctuator '^' vs bitwise or '^')
+    */
+    switch (code[i]){
+        case '!':
+            if(code[i]=='!' && code[i+1]=='='){
+                return false;
+            }
+            return true;
+        case '%':
+            if(code[i]=='%' && code[i+1]=='='){
+                return false;
+            }
+            return true;
+        case '^':
+            if(code[i]=='^' && code[i+1]=='='){
+                return false;
+            }
+            return true;
+        case '&':
+            if((code[i]=='&' && code[i+1]=='=')
+            || (code[i]=='&' && code[i+1]=='&')
+            || (code[i-1]=='&' && code[i]=='&')
+            || (code[i]=='&' && isalpha(code[i+1]))){
+                return false;
+            }
+            return true;
+        case '*':
+            if(code[i]=='*' && code[i+1]=='='){
+                return false;
+            }
+            return true;
+        case '(':
+            return true;
+        case ')':
+            return true;
+        case '-':
+            if((code[i]=='-' && code[i+1]=='=')
+            || (code[i]=='-' && code[i+1]=='-')
+            || (code[i-1]=='-' && code[i]=='-')){
+                return false;
+            }
+            return true;
+        case '+':
+            if((code[i]=='+' && code[i+1]=='=')
+            || (code[i]=='+' && code[i+1]=='+')
+            || (code[i-1]=='+' && code[i]=='+')){
+                return false;
+            }
+            return true;
+        case '<':
+            if((code[i]=='<' && code[i+1]=='=')
+            || (code[i]=='<' && code[i+1]=='<')
+            || (code[i-1]=='<' && code[i]=='<')){
+                return false;
+            }
+            return true;
+        case '>':
+            if((code[i]=='>' && code[i+1]=='=')
+            || (code[i]=='>' && code[i+1]=='>')
+            || (code[i-1]=='>' && code[i]=='>')){
+                return false;
+            }
+            return true;
+        case '=':
+            if((code[i]=='=' && code[i+1]=='=')
+            || (code[i-1]=='=' && code[i]=='=')){
+                return false;
+            }
+            return true;
+        case '{':
+            return true;
+        case '}':
+            return true;
+        case '|':
+            if((code[i]=='|' && code[i+1]=='=')
+            || (code[i]=='|' && code[i+1]=='|')
+            || (code[i-1]=='|' && code[i]=='|')){
+                return false;
+            }
+            return true;
+        case '~':
+            return true;
+        case '[':
+            return true;
+        case ']':
+            return true;
+        case '\\':
+            return true;
+        case ';':
+            return true;
+        case '\'':
+            return true;
+        case '?':
+            if(code[i]=='?' && code[i+1]==':'){
+                return false;
+            }
+            return true;
+        case ':':
+            if((code[i]==':' && code[i+1]==':')
+            || (code[i-1]==':' && code[i]==':')){
+                return false;
+            }
+            return true;
+        case '\"':
+            return true;
+        case ',':
+            return true;
+        case '.':
+            if(code[i]=='.' && code[i+1]=='*'){
+                return false;
+            }
+            return true;
+        case '/':
+            if(code[i]=='/' && code[i+1]=='='){
+                return false;
+            }
+            return true;
+        case '#':
+            return true;
+        default:
+            return false;
+    }
 }
