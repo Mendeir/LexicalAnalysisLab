82e405e008832130039b461fc5522402424db8db
MG <mg-git.plumbic@8shield.net>
2022-05-04 20:03:38 +0800

    Remove + and - as punctuator

--- main.cpp
+++ main.cpp
@@ -164,20 +164,6 @@ bool isPunctuator(string code, int i){
             return true;
         case ')':
             return true;
-        case '-':
-            if((code[i]=='-' && code[i+1]=='=')
-            || (code[i]=='-' && code[i+1]=='-')
-            || (code[i-1]=='-' && code[i]=='-')){
-                return false;
-            }
-            return true;
-        case '+':
-            if((code[i]=='+' && code[i+1]=='=')
-            || (code[i]=='+' && code[i+1]=='+')
-            || (code[i-1]=='+' && code[i]=='+')){
-                return false;
-            }
-            return true;
         case '<':
             if((code[i]=='<' && code[i+1]=='=')
             || (code[i]=='<' && code[i+1]=='<')
